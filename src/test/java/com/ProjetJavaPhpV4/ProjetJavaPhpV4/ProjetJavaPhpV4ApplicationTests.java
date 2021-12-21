97b68a88b4c3181d261120dd69d9e22236a23ad0
Romuald-Big-Screen <romuald.torro@gmail.com>
2021-12-21 16:26:14 +0100

    first commit

---
+++ src/test/java/com/ProjetJavaPhpV4/ProjetJavaPhpV4/ProjetJavaPhpV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjetJavaPhpV4.ProjetJavaPhpV4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJavaPhpV4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
